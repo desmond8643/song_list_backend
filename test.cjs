7cb961756d2c6ab869b214993d0468efdf7e2647
Desmond <desmondhei25@gmail.com>
2023-12-09 19:48:30 +0800

    first commit

---
+++ test.cjs
@@ -0,0 +1,53 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config({ path: './config.env' });
+
+const cors = require('cors');
+const app = express();
+const port = 3001; // Choose a port number for your server
+
+app.use(cors())
+
+// MongoDB connection and route
+app.get('/api/festivalplus', async (req, res) => {
+  const db = process.env.ATLAS_URI;
+  const client = new MongoClient(db);
+
+  try {
+    await client.connect();
+    const database = client.db('maimai');
+    const collection = database.collection('Festival+');
+
+    const documents = await collection.find().toArray();
+    res.json(documents);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  } finally {
+    await client.close();
+  }
+});
+
+app.get('/api/charts', async (req, res) => {
+  const db = process.env.ATLAS_URI;
+  const client = new MongoClient(db);
+
+  try {
+    await client.connect();
+    const database = client.db('maimai');
+    const collection = database.collection('charts');
+
+    const documents = await collection.find().toArray();
+    res.json(documents);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  } finally {
+    await client.close();
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
