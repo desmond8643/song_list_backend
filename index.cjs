c3bca3e925fc0328ab21f9ac59d1c0b06da3f584
Desmond <desmondhei25@gmail.com>
2023-12-30 18:53:18 +0800

    commit

---
+++ index.cjs
@@ -0,0 +1,114 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config({ path: './config.env' });
+
+const cors = require('cors');
+const app = express();
+const port = 443; // Choose a port number for your server
+
+app.use(cors())
+
+app.get('/home', (req, res) => {
+  res.status(200).json('Welcome, your app is working well');
+})
+
+// MongoDB connection and route
+app.get('/api/festivalplus', async (req, res) => {
+  const db = process.env.ATLAS_URI;
+  const client = new MongoClient(db);
+
+  try {
+    await client.connect();
+    const database = client.db('maimai');
+    const collection = database.collection('Festival+');
+
+    const documents = await collection.find().toArray();
+    res.json(documents);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  } finally {
+    await client.close();
+  }
+});
+
+app.get('/api/buddies', async (req, res) => {
+  const db = process.env.ATLAS_URI;
+  const client = new MongoClient(db);
+
+  try {
+    await client.connect();
+    const database = client.db('maimai');
+    const collection = database.collection('Buddies');
+
+    const documents = await collection.find().toArray();
+    res.json(documents);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  } finally {
+    await client.close();
+  }
+});
+
+app.get('/api/utage', async (req, res) => {
+  const db = process.env.ATLAS_URI;
+  const client = new MongoClient(db);
+
+  try {
+    await client.connect();
+    const database = client.db('maimai');
+    const collection = database.collection('utage');
+
+    const documents = await collection.find().toArray();
+    res.json(documents);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  } finally {
+    await client.close();
+  }
+});
+
+app.get('/api/charts', async (req, res) => {
+  const db = process.env.ATLAS_URI;
+  const client = new MongoClient(db);
+
+  try {
+    await client.connect();
+    const database = client.db('maimai');
+    const collection = database.collection('charts');
+
+    const documents = await collection.find().toArray();
+    res.json(documents);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  } finally {
+    await client.close();
+  }
+});
+
+app.get('/api/songs', async (req, res) => {
+  const db = process.env.ATLAS_URI;
+  const client = new MongoClient(db);
+
+  try {
+    await client.connect();
+    const database = client.db('maimai');
+    const collection = database.collection('songs');
+
+    const documents = await collection.find().toArray();
+    res.json(documents);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  } finally {
+    await client.close();
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
