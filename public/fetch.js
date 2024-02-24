13efb7ed4fe89c411c8ff7e58e2d01833448fc6a
Desmond <desmondhei25@gmail.com>
2024-02-24 22:22:13 +0800

    commit

--- public/fetch.js
+++ public/fetch.js
@@ -50,13 +50,13 @@
 //         const musicDivs = doc1.querySelectorAll(".w_450")
 
 //         const songs = []
-  
+
 //         musicDivs.forEach((musicDiv) => {
 //           const nameElement = musicDiv.querySelector(
 //             ".music_master_score_back .music_name_block"
 //           )
 //           const name = nameElement ? nameElement.textContent.trim() : ""
-  
+
 //           const scoreBlockElements = musicDiv.querySelectorAll(
 //             ".music_master_score_back .music_score_block"
 //           )
@@ -67,7 +67,7 @@
 //             ? scoreBlockElements[1].textContent
 //             : ""
 //           deluxeScore = deluxeScore.replace(/[ ,\n\t]/g, "")
-  
+
 //           const isDXChart =
 //             musicDiv.querySelector(".music_kind_icon_dx.music_master_btn_on") !==
 //             null
@@ -75,9 +75,9 @@
 //             musicDiv.querySelector(
 //               ".music_kind_icon_standard.music_master_btn_on"
 //             ) !== null
-          
+
 //             // songs.push({name, achievement, deluxeScore})
-  
+
 //           if (isDXChart) {
 //             // If both standard and DX icons are present, prefer DX chart
 //             songs.push({ name, achievement, deluxeScore, chart: "dx" })
@@ -158,26 +158,52 @@
 //     console.error("Error fetching name:", error)
 //   })
 
-const fetchData = async (url) => {
+const submitFormData = async (formData) => {
   try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+    const response = await fetch(
+      "https://song-list-backend.vercel.app/api/submit",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      }
+    )
+
+    if (response.ok) {
+      console.log("Form data sent successfully")
+      const responseData = await response.json()
+      console.log("Response from backend:", responseData)
+    } else {
+      throw new Error("Failed to send form data")
     }
-    const data = await response.text();
-    return data;
   } catch (error) {
-    console.error("Error fetching data:", error);
-    throw error;
+    console.error("An error occurred while sending form data:", error)
+    throw error
   }
-};
+}
+
+const fetchAndProcessData = async (url) => {
+  try {
+    const response = await fetch(url)
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`)
+    }
+    const data = await response.text()
+    return data
+  } catch (error) {
+    console.error("Error fetching data:", error)
+    throw error
+  }
+}
 
 const parseMasterData = (masterData) => {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(masterData, "text/html");
+  const parser = new DOMParser()
+  const doc = parser.parseFromString(masterData, "text/html")
 
-  const elements = doc.getElementsByClassName("f_10");
-  const stats = [];
+  const elements = doc.getElementsByClassName("f_10")
+  const stats = []
   const attributes = [
     "Clear",
     "S",
@@ -200,113 +226,110 @@ const parseMasterData = (masterData) => {
     "3",
     "4",
     "5",
-  ];
+  ]
 
   for (let i = 0; i < elements.length; i++) {
-    const value = elements[i].textContent;
-    const attribute = attributes[i];
-    stats.push({ value, attribute });
+    const value = elements[i].textContent
+    const attribute = attributes[i]
+    stats.push({ value, attribute })
   }
 
-  const musicDivs = doc.querySelectorAll(".w_450");
-  const songs = [];
+  const musicDivs = doc.querySelectorAll(".w_450")
+  const songs = []
 
   musicDivs.forEach((musicDiv) => {
     const nameElement = musicDiv.querySelector(
       ".music_master_score_back .music_name_block"
-    );
-    const name = nameElement ? nameElement.textContent.trim() : "";
+    )
+    const name = nameElement ? nameElement.textContent.trim() : ""
 
     const scoreBlockElements = musicDiv.querySelectorAll(
       ".music_master_score_back .music_score_block"
-    );
+    )
     const achievement = scoreBlockElements[0]
       ? scoreBlockElements[0].textContent
-      : "";
+      : ""
     let deluxeScore = scoreBlockElements[1]
       ? scoreBlockElements[1].textContent
-      : "";
-    deluxeScore = deluxeScore.replace(/[ ,\n\t]/g, "");
+      : ""
+    deluxeScore = deluxeScore.replace(/[ ,\n\t]/g, "")
 
     const isDXChart =
-      musicDiv.querySelector(".music_kind_icon_dx.music_master_btn_on") !==
-      null;
+      musicDiv.querySelector(".music_kind_icon_dx.music_master_btn_on") !== null
     const isStdChart =
       musicDiv.querySelector(
         ".music_kind_icon_standard.music_master_btn_on"
-      ) !== null;
+      ) !== null
 
     if (isDXChart) {
-      songs.push({ name, achievement, deluxeScore, chart: "dx" });
+      songs.push({ name, achievement, deluxeScore, chart: "dx" })
     } else if (isStdChart) {
-      songs.push({ name, achievement, deluxeScore, chart: "std" });
+      songs.push({ name, achievement, deluxeScore, chart: "std" })
     } else {
-      const imgElement = musicDiv.querySelector(".music_kind_icon");
-      const src = imgElement ? imgElement.getAttribute("src") : "";
+      const imgElement = musicDiv.querySelector(".music_kind_icon")
+      const src = imgElement ? imgElement.getAttribute("src") : ""
       if (src.includes("standard")) {
-        songs.push({ name, achievement, deluxeScore, chart: "std" });
+        songs.push({ name, achievement, deluxeScore, chart: "std" })
       } else if (src.includes("dx")) {
-        songs.push({ name, achievement, deluxeScore, chart: "dx" });
+        songs.push({ name, achievement, deluxeScore, chart: "dx" })
       }
     }
-  });
+  })
 
-  return { stats, songs };
-};
+  return { stats, songs }
+}
 
-const submitFormData = async (formData) => {
-  try {
-    const response = await fetch("https://song-list-backend.vercel.app/api/submit", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
+const processFormData = (formData, name, masterData, expertData) => {
+  formData[name] = {}
+  const masterDataObj = parseMasterData(masterData)
+  const expertDataObj = parseMasterData(expertData)
 
-    if (response.ok) {
-      console.log("Form data sent successfully");
-      const responseData = await response.json();
-      console.log("Response from backend:", responseData);
-    } else {
-      throw new Error("Failed to send form data");
-    }
-  } catch (error) {
-    console.error("An error occurred while sending form data:", error);
-    throw error;
-  }
-};
+  formData[name].masterData = masterDataObj.stats
+  formData[name].masterSongs = masterDataObj.songs
+  formData[name].expertData = expertDataObj.stats
+  formData[name].expertSongs = expertDataObj.songs
+}
 
-fetchData("https://maimaidx-eng.com/maimai-mobile/home/")
+fetchAndProcessData("https://maimaidx-eng.com/maimai-mobile/home/")
   .then((data) => {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(data, "text/html");
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(data, "text/html")
 
-    const element = doc.querySelector(".name_block");
-    const name = element ? element.textContent : "";
+    const element = doc.querySelector(".name_block")
+    const name = element ? element.textContent : ""
 
-    const formData = {
-      [name]: {} // Create an object with the username as the key
-    };
+    const formData = {}
 
     return Promise.all([
-      fetchData("https://maimaidx-eng.com/maimai-mobile/record/musicGenre/search/?genre=99&diff=3"),
-      fetchData("https://maimaidx-eng.com/maimai-mobile/record/musicGenre/search/?genre=99&diff=2")
+      fetchAndProcessData(
+        "https://maimaidx-eng.com/maimai-mobile/record/musicGenre/search/?genre=99&diff=3"
+      ),
+      fetchAndProcessData(
+        "https://maimaidx-eng.com/maimai-mobile/record/musicGenre/search/?genre=99&diff=2"
+      ),
     ]).then(([masterData, expertData]) => {
-      const masterDataObj = parseMasterData(masterData);
-      const expertDataObj = parseMasterData(expertData);
-
-      formData[name].masterData = masterDataObj.stats;
-      formData[name].masterSongs = masterDataObj.songs;
-      formData[name].expertData = expertDataObj.stats;
-      formData[name].expertSongs = expertDataObj.songs;
-
-      return formData;
-    });
+      processFormData(formData, name, masterData, expertData)
+      return formData
+    })
   })
   .then((formData) => {
-    return submitFormData(formData);
+    // Splitting the form data into smaller chunks
+    const chunkSize = 500 // Number of entries per chunk
+    const entries = Object.entries(formData)
+    const chunks = []
+
+    for (let i = 0; i < entries.length; i += chunkSize) {
+      const chunk = Object.fromEntries(entries.slice(i, i + chunkSize))
+      chunks.push(chunk)
+    }
+
+    // Submitting each chunk of form data
+    const submitPromises = chunks.map((chunk) => submitFormData(chunk))
+    return Promise.all(submitPromises)
+  })
+  .then(() => {
+    console.log("Form data submitted successfully")
   })
   .catch((error) => {
-    console.error("Error:", error);
-  });
+    console.error("Error:", error)
+  })
