a148701aebf2ba605d895a9724ad94e11bcec637
Desmond <desmondhei25@gmail.com>
2024-02-25 16:07:13 +0800

    commmit

--- public/fetch.js
+++ public/fetch.js
@@ -72,7 +72,7 @@ fetch("https://maimaidx-eng.com/maimai-mobile/home/")
 function getSongData(doc, difficulty) {
   const elements1 = doc.getElementsByClassName("f_10")
   const stats = []
-  const overallStats = ""
+  let overallStats = ""
   const attributes = [
     "Clear",
     "S",
